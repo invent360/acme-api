596be15017c8d07c8e847aeee7234080d03b6ba1
Nissi Tafie <ntafie@insys-icom.de>
2020-12-21 06:23:29 +0000

    first commit

---
+++ dependencies.sh
@@ -0,0 +1,12 @@
+go get -u github.com/go-openapi/errors
+go get -u  github.com/go-openapi/loads
+go get -u  github.com/go-openapi/runtime
+go get -u  github.com/go-openapi/spec
+go get -u  github.com/go-openapi/strfmt
+go get -u  github.com/go-openapi/swag
+
+go get -u golang.org/x/net/context
+go get -u github.com/go-openapi/errors
+go get -u github.com/go-openapi/strfmt
+go get -u github.com/go-openapi/swag
+go get -u github.com/go-openapi/validate
